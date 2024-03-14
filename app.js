201fe6a5d7b98f3d3ec088cce33588f1fa10451d
lord <lord@lord>
2024-03-14 18:56:14 +0530

    Added Delete operation

--- app.js
+++ app.js
@@ -98,3 +98,14 @@ app.put("/listings/:id/",async (req,res)=>{
     await Listing.findByIdAndUpdate(id ,{...req.body.listing});
     res.redirect(`/listings/${id}`);
 });
+
+
+//deleting any route 
+
+app.delete("/listings/:id/",async(req,res)=>{
+    let { id } = req.params; 
+    let deletedListing =  await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+});
+
