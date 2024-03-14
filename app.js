5ec21c81233252c8ea17ed6ea9935df75a00ad6c
lord <lord@lord>
2024-03-14 14:27:22 +0530

    Add new route

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express= require("express");
+const express = require("express");
 const app = express();
-const mongoose =require("mongoose");
-const Listing= require("./models/list.js")
-const path=require("path");
+const mongoose = require("mongoose");
+const Listing = require("./models/list.js")
+const path = require("path");
 
-app.listen(3000,(req,res)=>{
+app.listen(3000, (req, res) => {
     console.log("Running on port 3000");
 });
 
@@ -17,14 +17,14 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
-app.use(express.urlencoded({extended:true}));  //to use id feature the req.params one 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));  //to use id feature the req.params one 
 
 
 /*________BASICS SETUP DONE !_____________________________________________________________________________*/
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("hello root!");
 });
 
@@ -50,16 +50,35 @@ app.get("/",(req,res)=>{
 
 // });
 
-app.get("/listings",async (req,res)=>{    // allows to view all the lists available on the db 
+app.get("/listings", async (req, res) => {    // allows to view all the lists available on the db 
     const alllistings = await Listing.find({});
-    res.render("index.ejs",{alllistings});
+    res.render("index.ejs", { alllistings });
+});
+
+
+//creating a route for create a new list
+
+app.get("/listings/new", async (req, res) => {
+    res.render("new.ejs");
+});
+
+//creating the post requesitng after submutting the lisitngs 
+
+app.post("/listings/", async (req, res) => {
+/* method 1    let {title ,description ,image , price ,location ,country } = req.body      //taking anything from thr html form contains the information part in the req.body   */
+  const newListing = new Listing(req.body.listing);  // extracting the object from the html form of new.ejs then saving to db
+  await newListing.save();
+  res.redirect("/listings");  //redirecting succcessfully after the information is stored 
+
 });
 
 
 // show route 
 
-app.get("/listings/:id",async (req,res)=>{
-    let {id}= req.params;  //write the extended:true for this 
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;  //write the extended:true for this 
     const listing = await Listing.findById(id);  // finds the individual by id such as /listings/:id 
-    res.render("show.ejs",{listing:listing});
+    res.render("show.ejs", { listing: listing });
 });
+
+
