65210b685c8260863f3399572820850c20417a4b
lord <lord@lord>
2024-03-16 11:31:22 +0530

    majorprojectwith fronmtend done

--- models/list.js
+++ models/list.js
@@ -12,8 +12,8 @@ const listingSchema = new Schema({
       
         url:String,
         type: String,
-        // set: (v) => v === "" ? "https://images.unsplash.com/photo-1709713215976-a194717696eb?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
-        // default:"https://images.unsplash.com/photo-1709713215976-a194717696eb?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set: (v) => v === "" ? "https://images.unsplash.com/photo-1709713215976-a194717696eb?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+        default:"https://images.unsplash.com/photo-1709713215976-a194717696eb?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     price: Number,
     location: String,
